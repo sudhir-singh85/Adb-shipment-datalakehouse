ceb4232dcfc658f99cf6579491cf1e7e36146b6c
sudhir-singh85 <sudhirrsingh.parmar@gmail.com>
2024-07-16 12:17:41 +0530

    Add files via upload

---
+++ dev/configuration/configuration_notebook.py
@@ -0,0 +1,5 @@
+# Databricks notebook source
+raw_folder_path = '/mnt/storageaccountname/dev/src'
+bronze_folder_path = '/mnt/storageaccountname/dev/bronze' 
+silver_folder_path = '/mnt/storageaccountname/dev/silver' 
+gold_folder_path = '/mnt/storageaccountname/dev/gold' 
