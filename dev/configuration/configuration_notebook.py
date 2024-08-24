3ec458d441587c8b3ade805fd12429aab08b1867
sudhir-singh85 <sudhirrsingh.parmar@gmail.com>
2024-08-24 15:30:47 +0530

    Delete dev directory

--- dev/configuration/configuration_notebook.py
+++
@@ -1,5 +0,0 @@
-# Databricks notebook source
-raw_folder_path = '/mnt/storageaccountname/dev/src'
-bronze_folder_path = '/mnt/storageaccountname/dev/bronze' 
-silver_folder_path = '/mnt/storageaccountname/dev/silver' 
-gold_folder_path = '/mnt/storageaccountname/dev/gold' 
